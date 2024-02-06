be7548efe8f4346b152994c082f8ef7b53aadaca
Alice <Grayhild@mail.ru>
2024-02-06 14:35:33 +0700

    main_code

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
-from functions import cuts, data, count
+from functions import cuts, data, format_card, is_none, description, to_count, sum_
 
 listo = cuts()
 for i in range(len(listo)):
     dt = data(i, listo)
-    print(f"{dt:%d.%m.%Y}")
-    print(count(i, listo))
-    if count(i, listo) is not None:
-        card = count(i, listo)
+    des = description(i, listo)
+    print(f"{dt:%d.%m.%Y} {des}")
+    name_to, to_ = to_count(i, listo)
+    if is_none(i, listo):
+        name_card, num_card = format_card(i, listo)
+        print(f"{name_card} {num_card} -> {name_to} {to_}")
+    else:
+        print(f"{name_to} {to_}")
+    amount, name = sum_(i, listo)
+    print(f"{amount} {name}")
+    print(" ")
--- functions.py
+++ functions.py
@@ -9,8 +9,6 @@ def operations():
     return operations_json
 
 
-
-
 """"Сортировка списка по статусу "выполнено" И дате (сначала новые)"""
 def sort():
     operations_json = operations()
@@ -36,18 +34,55 @@ def data(i, listo):
     return dt
     #print(f"{dt:%d.%m.%Y}")
 
-"""def if_from(i, listo):
-    if listo[i].get("from") in listo:
-        return True
-    else:
-        return False"""
-
 
 """Получаем карту и счёт отправителя """
 def count(i, listo):
     count_ = listo[i].get("from")
     return count_
 
-def split_card(card):
-    #смотри решение на сайте
+"""Достаём, форматируем и шифруем номер карты"""
+def number_card(card):
+    card_number = card.split()[-1]
+    private_number = card_number[:6] + (len(card_number[6:-4]) * '*') + card_number[-4:]
+    chunks, chunk_size = len(private_number), len(private_number) // 4
+    number = " ".join([private_number[i:i + chunk_size] for i in range(0, chunks, chunk_size)])
+    return number
 
+"""Наименование карты (виза, мастеркард и тп)"""
+def name_card(card):
+    name = " ".join(card.split(' ')[:-1])
+    return name
+
+"""Проверка на наличие from"""
+def is_none(i, listo):
+    if count(i, listo) is not None:
+        return True
+    else:
+        return False
+
+"""При наличии from записывает и возвращает наименование и зашифрованный счёт карты"""
+def format_card(i, listo):
+    if is_none(i, listo):
+        card = count(i, listo)
+        number_card_ = number_card(card)
+        name_card_ = name_card(card)
+        return name_card_, number_card_
+
+"""Возвращает тип операции"""
+def description(i, listo):
+    des = listo[i]["description"]
+    return des
+
+"""Счёт отправителя\вклада, форматирование, вывод = наименование, номер в шифре"""
+def to_count(i, listo):
+    to = listo[i]["to"]
+    to_cut = to.split()[-1]
+    to_ = ''.join("**" + to_cut[:4])
+    name_to = ''.join(to.split(' ')[:-1])
+    return name_to, to_
+
+"""Достаём из списка сумму и валюту"""
+def sum_(i, listo):
+    amount = listo[i]["operationAmount"]["amount"]
+    name = listo[i]["operationAmount"]["currency"]["name"]
+    return amount, name
